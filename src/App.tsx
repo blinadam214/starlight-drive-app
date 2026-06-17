58c54db51fdba7fd3dcdb67153edb1093fcd2ab3
blinadam214 <blinadam2@gmail.com>
2026-06-18 00:34:06 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import StickyWhatsApp from "@/components/StickyWhatsApp";
 import VehicleDetail from "./pages/VehicleDetail";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
